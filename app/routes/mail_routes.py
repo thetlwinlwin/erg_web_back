9e23da3f6427c1e7ad0e975236e0d10b4e849011
thetlwinlwin <minthantlwin8@gmail.com>
2023-11-17 13:21:48 +0630

    create dependencies for sending mail

--- app/routes/mail_routes.py
+++ app/routes/mail_routes.py
@@ -1,8 +1,9 @@
 from smtplib import SMTPException
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
 
+from app.core import AppSettings, get_app_settings
 from app.mail_handler.email_sending import send_suggestion
 from app.schemas.suggestion_schema import Suggestion
 
@@ -10,9 +11,11 @@ email_router = APIRouter(prefix="/email")
 
 
 @email_router.post("/")
-def send_email(suggestion: Suggestion):
+def send_email(
+    suggestion: Suggestion, settings: AppSettings = Depends(get_app_settings)
+):
     try:
-        send_suggestion(item=suggestion)
+        send_suggestion(item=suggestion, settings=settings)
         return JSONResponse(content="success", status_code=status.HTTP_200_OK)
     except SMTPException as e:
         return JSONResponse(
