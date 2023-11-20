ba29c8cfc635b1609833dfd1c23626ea4265e52e
thetlwinlwin <minthantlwin8@gmail.com>
2023-11-20 22:31:52 +0630

    create routes and events

--- app/routes/event_routes.py
+++ app/routes/event_routes.py
@@ -1,14 +1,25 @@
 from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
 
 from app.g_service import GService, get_gservice
+from app.g_service.gdrive import GoogleDrive
 
 event_router = APIRouter(prefix="/events")
 
 
-@event_router.get("/")
-def get_events(g_service: GService = Depends(get_gservice)):
+@event_router.get("/update")
+def get_events(
+    g_service: GService = Depends(get_gservice),
+):
     service = g_service.get_drive()
-    results = service.files().list().execute()
-    # get the results
-    items = results.get("files", [])
-    print(items)
+    GoogleDrive(service).update_docs()
+    return JSONResponse(content="success", status_code=status.HTTP_200_OK)
+
+
+@event_router.get("/refresh")
+def refresh_events(
+    g_service: GService = Depends(get_gservice),
+):
+    service = g_service.get_drive()
+    GoogleDrive(service=service).refresh()
+    return JSONResponse(content="success", status_code=status.HTTP_200_OK)
