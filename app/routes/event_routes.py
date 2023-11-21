7f6414e5a7cc9db6689638fa532c0a14d40fbefa
thetlwinlwin <minthantlwin8@gmail.com>
2023-11-21 09:58:40 +0630

    create route for update

--- app/routes/event_routes.py
+++ app/routes/event_routes.py
@@ -1,25 +1,30 @@
 from fastapi import APIRouter, Depends, status
-from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse, JSONResponse
 
-from app.g_service import GService, get_gservice
-from app.g_service.gdrive import GoogleDrive
+from app.g_service import FieldNames, GoogleDrive, GService, get_gservice
 
 event_router = APIRouter(prefix="/events")
 
 
-@event_router.get("/update")
-def get_events(
-    g_service: GService = Depends(get_gservice),
-):
-    service = g_service.get_drive()
-    GoogleDrive(service).update_docs()
-    return JSONResponse(content="success", status_code=status.HTTP_200_OK)
+@event_router.get("/")
+def get_events():
+    return FileResponse("events.json")
 
 
 @event_router.get("/refresh")
-def refresh_events(
+def update_text(
     g_service: GService = Depends(get_gservice),
 ):
     service = g_service.get_drive()
-    GoogleDrive(service=service).refresh()
+    GoogleDrive(service).refresh()
+    return JSONResponse(content="success", status_code=status.HTTP_200_OK)
+
+
+@event_router.get("/refresh/{field_name}")
+def refresh_events(
+    field_name: FieldNames,
+    g_service: GService = Depends(get_gservice),
+):
+    service = g_service.get_drive()
+    GoogleDrive(service=service).update(field=field_name)
     return JSONResponse(content="success", status_code=status.HTTP_200_OK)
