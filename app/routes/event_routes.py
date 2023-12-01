bc69ace57a152acf13ef149da406701535644b33
thetlwinlwin <minthantlwin8@gmail.com>
2023-12-02 01:33:52 +0630

    integrate firebase into routes

--- app/routes/event_routes.py
+++ app/routes/event_routes.py
@@ -1,30 +1,26 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.responses import FileResponse, JSONResponse
 
-from app.g_service import FieldNames, GoogleDrive, GServiceFactory, get_gservice
+from app.firebase.crud import FirebaseCrud, get_firebase_crud
+from app.g_service import GoogleDrive, GServiceFactory, get_gservice
 
 event_router = APIRouter(prefix="/events")
 
 
 @event_router.get("/")
-def get_events():
-    return FileResponse("events.json")
+def get_events(
+    firebase_crud: FirebaseCrud = Depends(get_firebase_crud),
+):
+    data = firebase_crud.read()
+    return JSONResponse(content=data, status_code=status.HTTP_200_OK)
 
 
 @event_router.get("/refresh")
 def refresh_events(
     g_service: GServiceFactory = Depends(get_gservice),
+    firebase_crud: FirebaseCrud = Depends(get_firebase_crud),
 ):
     service = g_service.get_drive()
-    GoogleDrive(service).refresh()
-    return JSONResponse(content="success", status_code=status.HTTP_200_OK)
-
-
-@event_router.get("/refresh/{field_name}")
-def update_events_field(
-    field_name: FieldNames,
-    g_service: GServiceFactory = Depends(get_gservice),
-):
-    service = g_service.get_drive()
-    GoogleDrive(service=service).update(field=field_name)
+    data = GoogleDrive(service).refresh()
+    firebase_crud.write(data)
     return JSONResponse(content="success", status_code=status.HTTP_200_OK)
